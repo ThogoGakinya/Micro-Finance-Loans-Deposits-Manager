f1215ad9949f10a47ce98d6a3ec2c958ea4d6249
Alfred Sopia <sopiaah100@gmail.com>
2021-01-19 01:49:54 +0300

    feat(core): Initial Data Import
    Added payments and accounts table's migration scripts; db scripts to initialized accounts and import all payments for the year 2019 and 2020

---
+++ database/migrations/2021_01_18_192837_create_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('account_name')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
