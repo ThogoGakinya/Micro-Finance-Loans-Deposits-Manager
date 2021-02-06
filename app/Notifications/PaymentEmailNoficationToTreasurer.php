704d593d8136cbff77538877b68d64c1305a9472
Alfred Sopia <sopiaah100@gmail.com>
2021-02-07 02:45:23 +0300

    refactor(line): Little refactoring

--- app/Notifications/PaymentEmailNoficationToTreasurer.php
+++ app/Notifications/PaymentEmailNoficationToTreasurer.php
@@ -26,7 +26,7 @@ class PaymentEmailNoficationToTreasurer extends Notification
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function via($notifiable)
@@ -37,7 +37,7 @@ class PaymentEmailNoficationToTreasurer extends Notification
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
@@ -48,7 +48,7 @@ class PaymentEmailNoficationToTreasurer extends Notification
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
@@ -64,19 +64,19 @@ class PaymentEmailNoficationToTreasurer extends Notification
     public function getMessage()
     {
         $lastRecord = DB::table('payments')->orderBy('id', 'DESC')->first();
-        $account= DB::table('accounts')->where('id','=',$lastRecord->account_id)->first();
+        $account = DB::table('accounts')->where('id', '=', $lastRecord->account_id)->first();
         // $date = strtotime($lastRecord->created_at)->format('jS M-Y H:i:s');
         $date = date('jS M, Y', strtotime($lastRecord->created_at));
         $time = date('H:i:s', strtotime($lastRecord->created_at));
         return (new MailMessage)
-            ->subject(config('app.name') . ': New Payment of Ksh '.number_format($lastRecord->amount).' for '.$account->account_name) //Custom way
-            ->greeting('Hello,')
-            ->line( new HtmlString('We would like to inform you that a new Payment of ' .'<b>'. 'Ksh '.number_format($lastRecord->amount).'</b>'.' for <b>'.$account->account_name.'</b>'.' has been made on ' .'<b>'.$date.'</b>'.' at '.'<b>'.$time.'</b>'))
-            ->line(new HtmlString('<strong>MPESA Transaction ID: </strong><b style="color:green;">'.$lastRecord->transaction_id.'</b>'))
+            ->subject(config('app.name') . ': New Payment of Ksh ' . number_format($lastRecord->amount) . ' for ' . $account->account_name) //Custom way
+            ->greeting('Dear Treasurer,')
+            ->line(new HtmlString('We would like to inform you that a new Payment of ' . '<b>' . 'Ksh ' . number_format($lastRecord->amount)
+                . '</b>' . ' for <b>' . $account->account_name . '</b>' . ' has been made on ' . '<b>' . $date . '</b>' . ' at ' . '<b>' . $time . '</b> <hr/>'))
             ->line('Please log in to see more information.')
-            ->action(config('app.name'),config('app.url').'/admin/payments/'.$lastRecord->id)
-            ->line('Thank you')
-            ->line(new HtmlString('Regards, <br/> MUCU Accounts Team. <br/><br/>'))
-            ->salutation(new HtmlString('<font color="#888888" size="2"> Note: This is an automated email, please do not reply to it.</font>'));
+        ->action(config('app.name'), config('app.url') . '/admin/payments/' . $lastRecord->id)
+        ->line('Thank you')
+        ->line(new HtmlString('Regards, <br/> MUCU Accounts Team. <br/><br/>'))
+        ->salutation(new HtmlString('<font color="#888888" size="2"> Note: This is an automated email, please do not reply to it.</font>'));
     }
 }
