344bebc7aeb433bd00ff129a8e3373ab1d4a53ce
Alfred Sopia <sopiaah100@gmail.com>
2021-02-07 00:13:08 +0300

    feat(core): Payment Model Observer
    Added a payment model Observer. Reference: https://laravel.com/docs/8.x/eloquent#observers

---
+++ app/Observers/PaymentActionObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Payment;
+
+class PaymentActionObserver
+{
+    /**
+     * Handle the Payment "created" event.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return void
+     */
+    public function created(Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Handle the Payment "updated" event.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return void
+     */
+    public function updated(Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Handle the Payment "deleted" event.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return void
+     */
+    public function deleted(Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Handle the Payment "restored" event.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return void
+     */
+    public function restored(Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Handle the Payment "force deleted" event.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return void
+     */
+    public function forceDeleted(Payment $payment)
+    {
+        //
+    }
+}
