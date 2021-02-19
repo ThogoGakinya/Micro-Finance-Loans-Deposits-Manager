ffdee96c5644017597b67446a9abf26b6e9686fb
Alfred Sopia <sopiaah100@gmail.com>
2021-02-20 01:26:09 +0300

    feat(core): Minutes blade views
    Added Minutes blade views

---
+++ resources/views/admin/minutes/index.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            Minutes List
+            @can('minute_create')
+                <a class="btn btn-success float-right" href="{{ route('admin.minutes.create') }}">
+                    Upload Minutes
+                </a>
+            @endcan
+        </div>
+
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class=" table table-bordered table-striped table-hover datatable datatable-Minute">
+                    <thead>
+                    <tr>
+                        <th width="10">
+
+                        </th>
+                        <th>
+                            Minute For Date
+                        </th>
+                        <th>
+                            Document
+                        </th>
+                        <th>
+                            &nbsp;
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($minutes as $key => $minute)
+                        <tr data-entry-id="{{ $minute->id }}">
+                            <td>
+
+                            </td>
+                            <td>
+                                {{ $minute->minutes_date ?? '' }}
+                            </td>
+                            <td>
+                                @if($minute->minutes_document)
+                                    <a href="{{ $minute->minutes_document->getUrl() }}" target="_blank">
+                                        {{$minute->minutes_document->file_name}}
+                                    </a>
+                                @endif
+                            </td>
+                            <td>
+                              @can('minute_edit')
+                                    <a class="btn btn-xs btn-info"
+                                       href="{{ route('admin.minutes.edit', $minute->id) }}">
+                                        Edit
+                                    </a>
+                                @endcan
+
+                                @can('minute_delete')
+                                    <form action="{{ route('admin.minutes.destroy', $minute->id) }}" method="POST"
+                                          onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                          style="display: inline-block;">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <input type="submit" class="btn btn-xs btn-danger" value="Delete">
+                                    </form>
+                                @endcan
+
+                            </td>
+
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
+@section('scripts')
+    @parent
+    <script>
+        $(function () {
+            let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+
+
+            $.extend(true, $.fn.dataTable.defaults, {
+                orderCellsTop: true,
+                order: [[2, 'desc']],
+                pageLength: 10,
+            });
+            let table = $('.datatable-Minute:not(.ajaxTable)').DataTable({buttons: dtButtons})
+            /*$('a[data-toggle="tab"]').on('shown.bs.tab click', function (e) {
+                $($.fn.dataTable.tables(true)).DataTable()
+                    .columns.adjust();
+            });*/
+
+        })
+
+    </script>
+@endsection
