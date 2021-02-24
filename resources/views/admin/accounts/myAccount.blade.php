bfb8f6580ca5407c84cede24dc0a124cecdbe7c3
Alfred Sopia <sopiaah100@gmail.com>
2021-02-24 22:50:00 +0300

    style(look): Change myAccount page look

--- resources/views/admin/accounts/myAccount.blade.php
+++ resources/views/admin/accounts/myAccount.blade.php
@@ -1,62 +1,48 @@
 @extends('layouts.admin')
 @section('content')
 
-    <div class="card">
-        <div class="card-header">
-            Show Account
-        </div>
-        <div class="card-body">
-            <div class="form-group">
-                <div class="form-group">
-                    <a class="btn btn-default" href="{{ route('admin.accounts.index') }}">
-                        {{ trans('global.back_to_list') }}
-                    </a>
-                </div>
-                <table class="table table-bordered table-striped">
-                    <tbody>
-                    <tr>
-                        <th>Account ID</th>
-                        <td>{{ $account->id }}</td>
-                    </tr>
-                    <tr>
-                        <th> Account Name</th>
-                        <td>{{ $account->account_name }}</td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div class="form-group">
-                    <a class="btn btn-default" href="{{ route('admin.accounts.index') }}">
-                        {{ trans('global.back_to_list') }}
-                    </a>
-                </div>
+    <di>
+        @if (empty($account->id))
+            <div class="callout callout-danger">
+                <h5><i class="fas fa-info"></i> Note:</h5>
+                Dear <strong>{{ Auth::user()->name }}, </strong> There is no Account linked to you at the moment
             </div>
-        </div>
-    </div>
+        @else
+        <div class="card">
+            <table class="table table-responsive-sm table-striped">
+                <tbody>
+                <tr>
+                    <td>Account Number: {{  $account->id ?? ''}}</td>
+                    <td> Account Name: {{  $account->account_name ?? ''}}</td>
+                </tr>
+                </tbody>
+            </table>
+            <div class="card-header">
+                {{ trans('global.relatedData') }}
+            </div>
 
-    <div class="card">
-        <div class="card-header">
-            {{ trans('global.relatedData') }}
-        </div>
-        <ul class="nav nav-tabs" role="tablist" id="relationship-tabs">
-            <li class="nav-item">
-                <a class="nav-link" href="#account_users" role="tab" data-toggle="tab">
-                    Owner(s)/Contributor(s)
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#account_payments" role="tab" data-toggle="tab">
-                    Payments History
-                </a>
-            </li>
-        </ul>
-        <div class="tab-content">
-            <div class="tab-pane" role="tabpanel" id="account_users">
-                @includeIf('admin.accounts.relationships.accountUsers', ['users' => $account->accountUsers])
-            </div>
-            <div class="tab-pane" role="tabpanel" id="account_payments">
-                @includeIf('admin.accounts.relationships.accountPayments', ['payments' => $account->accountPayments])
+            <ul class="nav nav-tabs" role="tablist" id="relationship-tabs">
+                <li class="nav-item">
+                    <a class="nav-link" href="#account_users" role="tab" data-toggle="tab">
+                        Owner(s)/Contributor(s)
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#account_payments" role="tab" data-toggle="tab">
+                        Payments History
+                    </a>
+                </li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane" role="tabpanel" id="account_users">
+                    @includeIf('admin.accounts.relationships.accountUsers', ['users' => $account->accountUsers])
+                </div>
+                <div class="tab-pane" role="tabpanel" id="account_payments">
+                    @includeIf('admin.accounts.relationships.accountPayments', ['payments' => $account->accountPayments])
+                </div>
             </div>
         </div>
-    </div>
+        @endif
+    </di>
 
 @endsection
