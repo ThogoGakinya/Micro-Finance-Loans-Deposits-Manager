ee24728aa7d8a3001790665a2abdde4f915a6b04
Alfred Sopia <sopiaah100@gmail.com>
2021-03-05 22:38:09 +0300

    build(db): Added roles and permissions table

---
+++ z_db_scripts/roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO mucu_db.roles (id, title, created_at, updated_at, deleted_at) VALUES (1, 'Administrator', null, null, null);
+INSERT INTO mucu_db.roles (id, title, created_at, updated_at, deleted_at) VALUES (2, 'Treasurer', null, null, null);
+INSERT INTO mucu_db.roles (id, title, created_at, updated_at, deleted_at) VALUES (3, 'Secretary', '2021-01-21 22:21:02', '2021-01-21 22:21:02', null);
+INSERT INTO mucu_db.roles (id, title, created_at, updated_at, deleted_at) VALUES (4, 'Member', '2021-02-19 21:44:37', '2021-02-19 21:44:37', null);
